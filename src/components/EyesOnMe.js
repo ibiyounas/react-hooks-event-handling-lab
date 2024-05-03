56c45998711f93049a271c6a50acb1a365d94251
Ibrahem Younas <younasibrahem@gmail.com>
2024-05-03 14:20:37 -0400

    passes tests

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+function EyesOnMe() {
+    const handleFocus = (event) => {
+        event.preventDefault()
+        console.log("Good!")
+
+    }
+    const handleBlur = (event) => {
+        event.preventDefault();
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,23 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    const handleSubmit=(event) => {
+        event.preventDefault()
+        console.log("Entering password...")
+
+    }
+
     return (
-        <div></div>
+        <div>
+            <label>Passcode
+                <input onChange={handleSubmit}
+                id="passcode"
+                type="password"
+                placeholder="***"
+                />
+            </label>
+        </div>
     )
 }
 
